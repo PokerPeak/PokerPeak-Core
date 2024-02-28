dd6abb2f992853c86cc11746017b1f7b5a1210e3
saadabban76 <saadabban76@gmail.com>
2024-02-29 00:43:23 +0530

    sfd

--- client/src/components/Poker3dCard.jsx
+++ client/src/components/Poker3dCard.jsx
@@ -16,10 +16,10 @@ export function Poker3dCard() {
     <CardContainer className="inter-var relative">
       <CardBody
         className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]
-       w-auto sm:w-[30rem] h-auto rounded-xl -6 border "
+       w-auto sm:w-[30rem] h-auto rounded-xl p-6 border "
       >
         <div
-          className="bg-black
+          className="
         absolute -top-8 w-full z-10 "
         >
           <p
